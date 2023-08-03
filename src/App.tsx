fb9290590a31eac30b102f7777913969e7a492b6
Laize Ferraz <laizeferraz@gmail.com>
2023-08-03 19:40:01 +1000

    add header, global style and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components/Header'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <h1>hello</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Header />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
