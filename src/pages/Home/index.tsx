72868b7737b324c64cc0a4956e2ff1a1e225d662
Laize Ferraz <laizeferraz@gmail.com>
2023-08-03 19:59:43 +1000

    create timer and task form layouts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,56 @@
 import { Play } from 'phosphor-react'
 
-import { HomeContainer, StartCountdownButton } from './styles'
+import {
+  HomeContainer,
+  StartCountdownButton,
+  CountdownContainer,
+  Separator,
+  FormContainer,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <form>
+        <FormContainer>
+          <label htmlFor="task">I am going t work on</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give a name to your task"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Task 1" />
+            <option value="Task 2" />
+            <option value="Task 3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
         <StartCountdownButton type="submit">
           <Play size={24} />
-          Começar
+          Start
         </StartCountdownButton>
       </form>
     </HomeContainer>
